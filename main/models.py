00bcbf7c66e030fe7da0e7f96a68b0830ed4fc26
Lee Jae Yeon <bestfourteen14@gmail.com>
2023-08-05 18:57:54 +0900

    add detailInfo, detailCommon

--- main/models.py
+++ main/models.py
@@ -33,7 +33,7 @@ class Tour(models.Model):
     cat1 = models.ForeignKey(ServiceCode, on_delete=models.CASCADE, related_name='tour_cat1')
     cat2 = models.ForeignKey(ServiceCode, on_delete=models.CASCADE, related_name='tour_cat2')
     cat3 = models.ForeignKey(ServiceCode, on_delete=models.CASCADE, related_name='tour_cat3')
-    content_id = models.ForeignKey(ServiceCode, on_delete=models.CASCADE, related_name='tour')
+    content_id = models.CharField(max_length=40)
     content_type_id = models.ForeignKey(ServiceCode, on_delete=models.CASCADE, related_name='tour_type')
     first_image = models.URLField(max_length=1024)
     first_image2 = models.URLField(max_length=1024)
@@ -49,3 +49,21 @@ class Tour(models.Model):
     def __str__(self):
         return self.title
 
+class DetailInfo(models.Model):
+    id = models.AutoField(primary_key=True, null=False, blank=False)
+    info_name = models.CharField(max_length=40)
+    content_id = models.CharField(max_length=40)
+    content_type_id = models.ForeignKey(AreaCode, on_delete=models.CASCADE, related_name='detailinfo_type')
+    fidgubun = models.CharField(max_length=40)
+    info_text = models.TextField(default='')
+    serial_num = models.CharField(max_length=40)
+
+class DetailCommon(models.Model):
+    id = models.AutoField(primary_key=True, null=False, blank=False)
+    overview = models.TextField(default='')
+    tel_Name = models.CharField(max_length=50)
+    title = models.CharField(max_length=90)
+    modified_time = models.CharField(max_length=50)
+    homepage = models.TextField(default='')
+    content_id = models.ForeignKey(ServiceCode, on_delete=models.CASCADE, related_name='detailcommon_contentid')
+    cpyrhtDivCd = models.TextField(default='')
