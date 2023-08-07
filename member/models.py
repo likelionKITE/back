edc0d2c999616d266fa8b1804ee0bf18181174be
KimSuYun0723 <102517937+KimSuYun0723@users.noreply.github.com>
2023-08-08 04:33:25 +0900

    member.models & .serializers _ first commit

--- member/models.py
+++ member/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    REQUIRED_FIELDS = []
+    email = None
+    nickname = models.CharField(max_length=100)
+
+    #여기서 related_name 설정을 안해주면 계속 역참조 에러가 남.
+    groups = models.ManyToManyField(Group, related_name='custom_users')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_users_permissions')
