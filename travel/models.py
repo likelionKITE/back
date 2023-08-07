96e8da37f4f92d042edad8e376a7c7d9d831e085
Zena0128 <0128ja@gmail.com>
2023-08-07 21:06:12 +0900

    alter festival&travel table

--- travel/models.py
+++ travel/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 # Create your models here.
-from main.models import ServiceCode
+
+from main.models import Tour
 
 
 class DetailIntroTravel(models.Model):
     id = models.AutoField(primary_key=True, null=False, blank=False)
-    content_id = models.ForeignKey(ServiceCode, on_delete=models.CASCADE, related_name='detail_intro_travel')
+    content_id = models.ForeignKey(Tour, on_delete=models.CASCADE, related_name='detail_intro_travel')
     heritage1 = models.CharField(max_length=1)
     accom_count = models.CharField(max_length=10)
     exp_age_range = models.CharField(max_length=10)
