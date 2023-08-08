fbb2816e7a9b2d4cce746fff492d417be4a30adc
KimSuYun0723 <102517937+KimSuYun0723@users.noreply.github.com>
2023-08-09 02:34:55 +0900

    suyun commit2

--- KITE_back/urls.py
+++ KITE_back/urls.py
@@ -19,6 +19,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('member/', include('dj_rest_auth.urls')),
-    path('member/signup/', include('dj_rest_auth.registration.urls')),
 ]
