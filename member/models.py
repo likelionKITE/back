73141c28ddda6d7e420aa91c0adc0cea3a482d9d
KimSuYun0723 <102517937+KimSuYun0723@users.noreply.github.com>
2023-08-08 17:06:26 +0900

    8/8 error solved

--- member/models.py
+++ member/models.py
@@ -7,7 +7,3 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = []
     email = None
     nickname = models.CharField(max_length=100)
-
-    #여기서 related_name 설정을 안해주면 계속 역참조 에러가 남.
-    groups = models.ManyToManyField(Group, related_name='custom_users')
-    user_permissions = models.ManyToManyField(Permission, related_name='custom_users_permissions')
