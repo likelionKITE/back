deff41813f3a48cd2d7f41e1823011f256a1322f
Lee Jae Yeon <bestfourteen14@gmail.com>
2023-08-10 18:01:02 +0900

    modified CitySerializer

--- city/serializers.py
+++ city/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from main.models import Tour
+from main.models import Tour, AreaCode, ServiceCode, DetailCommon
 
 # Create your serializers here.
+
 class CitySerializer(serializers.ModelSerializer):
+    area_code = serializers.CharField(source="sigungu_code.name", read_only=True)
+    sigungu_code = serializers.CharField(source="sigungu_code.sigungu_name", read_only=True)
+    cat1 = serializers.CharField(source="cat3.main_name", read_only=True)
+    cat2 = serializers.CharField(source="cat3.mid_name", read_only=True)
+    cat3 = serializers.CharField(source="cat3.sub_name", read_only=True)
+
     class Meta:
         model = Tour
         fields = ["id", 
--- city/urls.py
+++ city/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import *
 
-app_name = 'movie'
+app_name = 'city'
 
 urlpatterns = [
     path('list/', CityListView.as_view(), name='city_list'),
+    path('detail/<int:content_id>/', CityDetailView.as_view(), name='city_detail'),
 ]
