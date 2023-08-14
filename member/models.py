7a630f808a8f4fc15be75662192b08023b4f6359
SuYunKim <202200737@hufs.ac.kr>
2023-08-14 17:51:29 +0900

    model_notcompleted

--- member/models.py
+++ member/models.py
@@ -1,7 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **kwargs):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=email,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        """주어진 이메일, 비밀번호 등 개인정보로 User 인스턴스 생성
+        단, 최상위 사용자 이므로 권한을 부여
+        """
+        superuser = self.create_user(
+            email=email,
+            password=password,
+        )
+        
+        superuser.is_staff = True
+        superuser.is_superuser = True
+        superuser.is_active = True
+        
+        superuser.save(using=self._db)
+        return superuser
 
 class CustomUser(AbstractUser):
     REQUIRED_FIELDS = []
